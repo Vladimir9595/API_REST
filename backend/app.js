d03521c4986b163f97697e8afd23b4863284d2b7
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-11-05 20:09:07 +0100

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require('express') // C'est la manière dont JS gère les modules. Express gère les routes
 
-// const Thing = require('./models/Thing')
-
 const stuffRoutes = require('./routes/stuff')
 const userRoutes = require('./routes/user')
+const path = require('path')
 
 const mongoose = require('mongoose')
 
@@ -36,47 +35,6 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes)
-
-// app.post('/api/stuff/', (req, res, next) => {
-//   delete req.body._id
-//   const thing = new Thing({
-//     ...req.body,
-//   })
-//   thing
-//     .save()
-//     // Fonctions asynchrone = async function - JavaScript fonctions promise
-//     .then(() => res.status(201).json({ message: 'Objet enregistré !' })) // Fonction flachée - envoie la réponse code ok 200
-//     .catch((error) => res.status(400).json({ error })) // Envoie l'erreur 400
-// })
-
-// // Permet de créer une route pour Modifier l'objet ou le post avec app.put et Thing.updateOne
-// app.put('/api/stuff/:id', (req, res, next) => {
-//   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-//     .then(() =>
-//       res.status(200).json({ message: 'Objet modifié avec succes !!' }),
-//     )
-//     .catch(() => res.status(400).json({ error }))
-// })
-
-// app.delete('/api/stuff/:id', (req, res, next) => {
-//   Thing.deleteOne({ _id: req.params.id })
-//     .then(() =>
-//       res.status(200).json({ message: 'Objet supprimé avec succes !' }),
-//     )
-//     .catch((error) => res.status(400).json({ error }))
-// })
-
-// // Rend le site dynamique on pouvant cliquer sur l'objet et voir les infoirmations
-// app.get('/api/stuff/:id', (req, res, next) => {
-//   Thing.findOne({ _id: req.params.id })
-//     .then((things) => res.status(200).json(things))
-//     .catch((error) => res.status(400).json({ error }))
-// })
-
-// app.get('/api/stuff/', (req, res, next) => {
-//   Thing.find()
-//     .then((things) => res.status(200).json(things))
-//     .catch((error) => res.status(400).json({ error }))
-// })
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 module.exports = app // Exportation du module
